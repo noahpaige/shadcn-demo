402a5811a66a3a7781577d680ef868be37347397
Noah Paige <noahlandonpaige@gmail.com>
2024-12-05 10:33:28 -0800

    Updates

--- src/components/pages/Opcon/pages/LaunchfeedPage/LaunchfeedPage.vue
+++
@@ -1,7 +0,0 @@
-<template className="flex min-h-screen flex-col items-center justify-between p-24">
-    <PostTable />
-</template>
-
-<script setup>
-import PostTable from './components/PostTable.vue';
-</script>
